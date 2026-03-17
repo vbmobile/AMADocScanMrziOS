e4861fc8316b3e1900fc30b33d989de236197cc8
Pedro Ferreira <pedro.ferreira@amadeus.com>
2026-03-17 11:55:28 +0000

    test name

--- Package.swift
+++ Package.swift
@@ -2,13 +2,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "AMADocScanMrziOS",
+    name: "mdi-mob-sdk-doc-scanner-ios",
     platforms: [
         .iOS(.v15)
     ],
     products: [
         .library(
-            name: "AMADocScanMrziOS",
+            name: "mdi-mob-sdk-doc-scanner-ios",
             targets: ["AMADocScanMrziOSWrapper"]
         )
     ],
@@ -18,13 +18,13 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "AMADocScanMrziOS",
+            name: "mdi-mob-sdk-doc-scanner-ios",
             url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocScanMrziOS/mdi-mob-sdk-doc-scanner-ios-1.0.0-rc01.zip",
             checksum: "7431763ae16046891a0f273f45ab13efee107817be6443094a4926afb11ec814"
         ),
         .target(
             name: "AMADocScanMrziOSWrapper",
-            dependencies: ["AMADocScanMrziOS",
+            dependencies: ["mdi-mob-sdk-doc-scanner-ios",
                            .product(name: "AMADocModeliOS", package: "AMADocModeliOS"),
                            .product(name: "AMADocScanneriOS", package: "AMADocScanneriOS"),],
             path: "Sources",
