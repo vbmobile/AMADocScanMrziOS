d76fdace309de3faeded0a6c907e88c77ca29179
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-08-13 11:25:21 +0100

    Version 2.0.4

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/vbmobile/AMADocModeliOS", .upToNextMinor(from: "2.0.0")),
-        .package(url: "https://github.com/vbmobile/AMADocScanneriOS", .exact("4.1.13-hotfix"))
+        .package(url: "https://github.com/vbmobile/AMADocModeliOS", .upToNextMinor(from: "2.0.2")),
+        .package(url: "https://github.com/vbmobile/AMADocScanneriOS", .exact("4.1.13"))
     ],
     targets: [
         .binaryTarget(
             name: "AMADocScanMrziOS",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocScanMrziOS/AMADocScanMrziOS-2.0.3.zip",
-            checksum: "3b19542d106dafe73970ca1baa408a06851014dd90916abc3516bc293908f62e"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocScanMrziOS/AMADocScanMrziOS-2.0.4.zip",
+            checksum: "d3b93f11167b8b88bd7ce265e2400686750fd0f44ecff8a10c01b7c0ed457943"
         ),
         .target(
             name: "AMADocScanMrziOSWrapper",
