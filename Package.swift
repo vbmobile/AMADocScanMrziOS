ff83f1ea1bb55c1c578f19f7e58ef440551dee30
ricardopatriciodossantos <ricardo.dossantos@amadeus.com>
2026-08-17 15:14:27 +0100

    Update AMADocScanMrziOS 2.0.5 checksum

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vbmobile/AMADocModeliOS", .upToNextMinor(from: "2.0.2")),
-        .package(url: "https://github.com/vbmobile/AMADocScanneriOS", .exact("4.1.13"))
+        .package(url: "https://github.com/vbmobile/AMADocScanneriOS", .exact("5.0.2"))
     ],
     targets: [
         .binaryTarget(
             name: "AMADocScanMrziOS",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocScanMrziOS/AMADocScanMrziOS-2.0.4.zip",
-            checksum: "d3b93f11167b8b88bd7ce265e2400686750fd0f44ecff8a10c01b7c0ed457943"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocScanMrziOS/AMADocScanMrziOS-2.0.5.zip",
+            checksum: "ae94a58fee313db8e1ca54daa21c465e4e1a13b05250256d711bf70ce42b5f7a"
         ),
         .target(
             name: "AMADocScanMrziOSWrapper",
